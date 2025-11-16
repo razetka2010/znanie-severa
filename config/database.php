c13d0e41c60aa3385a957e09b7b98897725e998c
razetka <128233067+razetka2010@users.noreply.github.com>
2025-11-17 04:59:10 +0500

    Create database.php

---
+++ config/database.php
@@ -0,0 +1,16 @@
+<?php
+function getDatabaseConnection() {
+    $host = 'localhost';
+    $dbname = 'school_diary';
+    $username = 'root';
+    $password = '';
+
+    try {
+        $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        return $pdo;
+    } catch (PDOException $e) {
+        die("Ошибка подключения к базе данных: " . $e->getMessage());
+    }
+}
+?>
